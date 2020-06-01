5bd463c1706425492c2e0f76470e3e39b69c2a43
Glen Gunawan <60954910+glengunawan@users.noreply.github.com>
2020-06-01 23:09:40 +0700

    Purwadhika Final Project
    Purwadhika Final Project

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -16,8 +16,6 @@ dbku = mysql.connector.connect(
 
 app = Flask(__name__) 
 
-# model = keras.models.load_model('./model2/trained_heart_model.h5')
-
 # ---Login---
 
 @app.route('/', methods=['GET', 'POST']) 
@@ -25,25 +23,30 @@ def home():
     if request.method == 'GET':
         return render_template('login.html') 
     elif request.method == 'POST': 
-        username = request.form['username']
-        password = request.form['password']
+        nama_pasien = request.form['name'].upper()
+        id_pasien = request.form['id']
 
-        x = dbku.cursor(dictionary=True) 
+        data_pasien = {'nama': nama_pasien, 'id': id_pasien}
 
-        query = 'select * from ListUser' 
+        x = dbku.cursor()
+        query = 'CREATE TABLE IF NOT EXISTS PatientList (name varchar(50), patientID varchar(50));'
         x.execute(query) 
 
-        tampungUser = list(x) 
-        listUsername = []  
-        listPassword = []  
+        x = dbku.cursor(dictionary=True) 
+        query = 'select * from PatientList' 
+        x.execute(query) 
 
-        for i in tampungUser: 
-            listUsername.append(i['Nama'])
-            listPassword.append(i['Password'])
+        tampungPasien = list(x) 
+        listNama = []  
+        listID = []  
 
-        if username in listUsername and password == listPassword[listUsername.index(username)]:
-            return redirect(url_for('form', user=username))
-            # return render_template('form.html')
+        for i in tampungPasien: 
+            listNama.append(i['name'])
+            listID.append(i['patientID'])
+
+
+        if nama_pasien in listNama and id_pasien == listID[listNama.index(nama_pasien)]:
+            return render_template('form.html', patient=data_pasien)
         else:
             return render_template('error_login.html')
 
@@ -54,47 +57,47 @@ def signup():
     if request.method == 'GET':
         return render_template('signup.html') 
     elif request.method == 'POST': 
-        username = request.form['username']
-        password = request.form['password']
+        name = request.form['name'].upper()
+        patientID = request.form['id']
+        
+        x = dbku.cursor()
+        query = 'CREATE TABLE IF NOT EXISTS PatientList (name varchar(50), patientID varchar(50));'
+        x.execute(query) 
 
         x = dbku.cursor(dictionary=True) 
 
-        query = 'select * from ListUser' 
+        query = 'select * from PatientList' 
         x.execute(query) 
 
-        tampungUser = list(x) 
-        listUsername = []  
-        listPassword = []  
+        tampungPasien = list(x) 
+        listPasien = []  
+        listID = []  
 
-        for i in tampungUser: 
-            listUsername.append(i['user'])
+        for i in tampungPasien: 
+            listPasien.append(i['name'])
 
-        if username in listUsername:
+        if name in listPasien:
             return render_template('error_signup.html')
         else:
-            dataUser = (username, password)
-            query = 'CREATE TABLE IF NOT EXISTS ListUser (user varchar(50), password varchar(50));'
-        
-            x = dbku.cursor()
-            x.execute(query) 
+            dataPasien = (name, patientID)
 
-            queryku = 'insert into ListUser (user, Password) values(%s, %s)' 
-            x.execute(queryku, dataUser) 
+            queryku = 'insert into PatientList (name, patientID) values(%s, %s)' 
+            x.execute(queryku, dataPasien) 
             dbku.commit()
 
             return render_template('proceed_signup.html')
 
 # --Form--
 
-@app.route('/form/<string:user>')
-def form(user):
-    return render_template('form.html', user=user) 
+# @app.route('/form/<string:user>')
+# def form(user):
+#     return render_template('form.html', user=user) 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     body = request.form 
 
-    user = body['user']
+    name = body['user'].upper()
     age = int(body['age'])
     sex = int(body['sex'])
     cp = int(body['cp'])
@@ -110,38 +113,76 @@ def predict():
     thal = int(body['thal']) 
 
     list_data = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
-    tuple_data = (age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
-
     dfData = pd.DataFrame(list_data)
 
-    print(list_data)
-    # querytable = 'CREATE TABLE IF NOT EXISTS ListData (user varchar(50), age int, sex int, cp int, trestbps int, chol int, fbs int, restecg int, thalach int, exang int, oldpeak int, slope int, ca int, thal int, proba float);'
-    # x = dbku.cursor()
-    # x.execute(query) 
-
-    # Prediksi
-
+    # Ambil ID
+    tuple_name = (name,)
+    x = dbku.cursor(dictionary=True) 
+    query = 'select patientID from PatientList where name = %s' 
+    x.execute(query, tuple_name) 
+    id_pasien = list(x)
+    id_pasien = id_pasien[0]['patientID']
+    
+    # Data Normalization and Preprocessing
     scaler = MinMaxScaler() 
-
     data_scale = scaler.fit_transform(dfData) 
     list_data_scale = list(data_scale) 
     
     list_tampung = []
     for i in list_data_scale: 
         list_tampung.append(i[0])
-
     dfData_scale = pd.DataFrame([list_tampung])
-    # print(list_tampung)
-    print(dfData_scale)
 
+    # Model Prediction 
     model = keras.models.load_model('./model2/trained_heart_model.h5')
     prediction = model.predict_classes(dfData_scale)
     probability = model.predict(dfData_scale) 
 
-    print(prediction, probability)
+    #Untuk Webpage
+    tuple_data = (name, id_pasien, age, sex, cp, trestbps, chol, 
+                fbs, restecg, thalach, exang, oldpeak, slope, 
+                ca, thal, prediction[0], probability[0][0])
 
+    #Khusus untuk SQL
+    tuple_data2 = (name, id_pasien, age, sex, cp, trestbps, chol, 
+                fbs, restecg, thalach, exang, oldpeak, slope, 
+                ca, thal, int(prediction[0]), float(probability[0][0]))
 
-    return "sukses"
+    querytable = 'CREATE TABLE IF NOT EXISTS ListData3 (name varchar(50), id varchar(50), age int, sex int, cp int, trestbps int, chol int, fbs int, restecg int, thalach int, exang int, oldpeak float, slope int, ca int, thal int, pred int, proba float);'
+    x = dbku.cursor()
+    x.execute(querytable) 
+    dbku.commit()
+
+    querydata = 'INSERT INTO ListData3 values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    x.execute(querydata, tuple_data2) 
+    dbku.commit()
+
+    # Ubah data untuk ditampilkan di HTML
+    list_column = ['name', 'id', 'age', 'sex', 'cp', 'trestbps', 'chol', 
+                    'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 
+                    'slope', 'ca', 'thal', 'prediction', 'probability']
+
+    list_data_lengkap = list(tuple_data) 
+
+    tampung_zip = zip(list_column, list_data_lengkap) 
+    dict_data = dict(tampung_zip)
+
+    return render_template('prediction.html', data=dict_data)
+
+@app.route('/history')
+def history():  
+    # Ambil ID
+    tuple_id = (12345,)
+    x = dbku.cursor(dictionary=True) 
+    query = 'select * from listData3 where id = %s' 
+    x.execute(query, tuple_id) 
+    data_pasien = list(x)
+    data = pd.DataFrame(data_pasien)
+
+    print(data)
+
+    return render_template('history.html',  tables=[data.to_html()], titles=data.columns.values, pasien = data_pasien)
+
 
 if __name__ == '__main__': 
     app.run(debug=True)
